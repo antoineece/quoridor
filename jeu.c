22693d5f30d85121b527f801045474d4f70c7b87
adrienlss <adrien.lassus@edu.ece.fr>
2024-11-24 11:57:18 +0100

    intialisation du plateau

--- jeu.c
+++ jeu.c
@@ -1,5 +1,18 @@
-//
-// Created by antoi on 23/11/2024.
-//
+#ifndef PLATEAU_H
+#define PLATEAU_H
 
-#include "jeu.h"
+#define TAILLE_PLATEAU 9
+#define MURS 10
+
+typedef struct {
+    char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU];
+    int joueur1_x, joueur1_y;
+    int joueur2_x, joueur2_y;
+    int murs_joueur1, murs_joueur2;
+    int scores_joueur1, scores_joueur2;
+} Jeu;
+
+void initialiser_plateau(Jeu *jeu);
+void afficher_plateau(Jeu *jeu);
+
+#endif // PLATEAU_H
