bddecac591d2446a1552e9ba4f3f9ac0aa53be14
Rochdgrvrn <roch.leperedegraveron@edu.ece.fr>
2024-11-24 22:14:19 +0100

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,8 +1,14 @@
-//
-// Created by antoi on 23/11/2024.
-//
-
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
 
-#endif //SAUVEGARDE_H
+
+#include "plateauint.h"
+
+
+void sauvegarder_partie(const Jeu *jeu);
+void charger_partie(Jeu *jeu);
+void menu(Jeu *jeu);
+
+
+#endif // SAUVEGARDE_H
+
