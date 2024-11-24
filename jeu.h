52facf92d28e68d40bebc679436baca89723cb6f
antoineece <antoine.dupuy@edu.ece.fr>
2024-11-24 22:21:45 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -5,16 +5,15 @@
 #include "plateauint.h"
 
 
-// Fonction pour déplacer un joueur
 void jouer_deplacement(Jeu *jeu, int joueur, int x, int y);
 
 
-// Fonction pour placer un mur
+
 void jouer_mur(Jeu *jeu, int joueur, int x1, int y1, int x2, int y2);
 
 
-// Vérifie si un joueur a gagné
+
 int verifier_gagnant(const Jeu *jeu);
 
 
-#endif // JEU_H
+#endif 
