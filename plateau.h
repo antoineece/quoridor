bf99b7e442797667aa637be84b2f1d6605dbefee
adrienlss <adrien.lassus@edu.ece.fr>
2024-11-28 16:28:51 +0100

    Rectification du dernier mauvais commit (.C au lieu de .h)

--- plateau.h
+++ plateau.h
@@ -1,37 +1,22 @@
-#include "plateauint.h"
-#include <stdio.h>
+#ifndef PLATEAUX_H
+#define PLATEAUX_H
 
 
-void initialiser_plateau(Jeu *jeu) {
-   for (int i = 0; i < TAILLE_PLATEAU; i++) {
-       for (int j = 0; j < TAILLE_PLATEAU; j++) {
-           jeu->plateau[i][j] = ' ';
-       }
-   }
-   jeu->joueur1_x = 0; jeu->joueur1_y = 4;
-   jeu->joueur2_x = 8; jeu->joueur2_y = 4;
-   jeu->murs_joueur1 = MURS;
-   jeu->murs_joueur2 = MURS;
-   jeu->scores_joueur1 = 0;
-   jeu->scores_joueur2 = 0;
+#define TAILLE_PLATEAU 9
+#define MURS 10
 
 
-   jeu->plateau[jeu->joueur1_x][jeu->joueur1_y] = '1';
-   jeu->plateau[jeu->joueur2_x][jeu->joueur2_y] = '2';
-}
+typedef struct {
+    char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU];
+    int joueur1_x, joueur1_y;
+    int joueur2_x, joueur2_y;
+    int murs_joueur1, murs_joueur2;
+    int scores_joueur1, scores_joueur2;
+} Jeu;
 
 
-void afficher_plateau(const Jeu *jeu) {
-   printf("\n  ");
-   for (int j = 0; j < TAILLE_PLATEAU; j++) {
-       printf("%d ", j);
-   }
-   printf("\n");
-   for (int i = 0; i < TAILLE_PLATEAU; i++) {
-       printf("%d ", i);
-       for (int j = 0; j < TAILLE_PLATEAU; j++) {
-           printf("%c ", jeu->plateau[i][j]);
-       }
-       printf("\n");
-   }
-}
+void initialiser_plateau(Jeu *jeu);
+void afficher_plateau(const Jeu *jeu);
+
+
+#endif
